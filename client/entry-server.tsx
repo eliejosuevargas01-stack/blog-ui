cdc3c35bf0696cde2d0cbc8845c2ded3cfee2c9f
eliezer josue vargas <eliejosuevargas01@gmail.com>
2026-01-08 13:09:21 -0300

    fix ssr helmet provider import

--- client/entry-server.tsx
+++ client/entry-server.tsx
@@ -1,9 +1,11 @@
 import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
-import { HelmetProvider } from "react-helmet-async";
+import * as ReactHelmetAsync from "react-helmet-async";
 
 import { AppRoutes } from "@/AppRoutes";
 import { AppShell } from "@/AppShell";
+
+const { HelmetProvider } = ReactHelmetAsync;
 
 export function render(url: string) {
   const helmetContext: { helmet?: { toString?: () => string } } = {};
